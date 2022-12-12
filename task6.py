e193f38fb0ca1cea2bfe4851a76b043f9f45805f
radugaboost <113132811+radugaboost@users.noreply.github.com>
2022-12-12 11:15:05 +0300

    Added correct algorithm

--- task6.py
+++ task6.py
@@ -12,3 +12,17 @@ def rob(nums):
         return 0
     else:
         return max(checker_rob(nums[1:]),checker_rob(nums[:-1]))
+
+def rob(nums):
+    def rob_row(nums):    
+        money = [0]*len(nums)
+        money[0] = nums[0]
+        money[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            money[i] = max(money[i-1], money[i-2] + nums[i])
+        return money[-1]
+    if not nums: 
+        return 0
+    if len(nums) <= 2: 
+        return max(nums)
+    return max(rob_row(nums[1:]), rob_row(nums[:-1]))    
